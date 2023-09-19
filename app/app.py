fc293838f5648046164446a4a4174135bb2c37e7
Raz-Dahan <razdahan31@gmail.com>
2023-09-19 14:37:08 +0300

    Remove Redis

--- app/app.py
+++ app/app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 from datetime import date
 import requests
 import os
-from redis import Redis
-
-redis = Redis(host='redis', port=6379)
 
 app = Flask(__name__)
 
@@ -13,15 +10,13 @@ def index():
     logo_url = "nasaLogo.png"
     icon_url = "nasaICON.png"
     today_data = get_nasa_image(date.today().strftime("%Y-%m-%d"))
-    redis.hincrby('entrance_count', 'total', 1)
-    count = redis.hget('entrance_count', 'total').decode('utf-8')
 
     if request.method == 'POST':
         request_date = request.form['date']
         image_url = get_nasa_image(request_date)['url']
-        return render_template('index.html', image_url=image_url, today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url, count=int(count))
+        return render_template('index.html', image_url=image_url, today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url)
     else:
-        return render_template('index.html', today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url, count=int(count)) 
+        return render_template('index.html', today_image_url=today_data['url'],today_describe_url=today_data['title'], logo_url=logo_url, icon_url=icon_url) 
 
 def get_nasa_image(date):
     secret_key = os.getenv('API_KEY')
